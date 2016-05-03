65269b5bb824be0bbdc29616533b407a267f2894
Joshua Friend <joshwhatk@gmail.com>
2016-05-03 10:01:36 -0600

    set up the contracts

--- src/Contracts/OrderableContract.php
+++ src/Contracts/OrderableContract.php
@@ -4,5 +4,17 @@ namespace JoshWhatK\Eloquent\Extensions\Contracts;
 
 interface OrderableContract
 {
-  //
+    /**
+     * Get the column mappings from the Model
+     *
+     * @return array
+     */
+    public function columnMaps();
+
+    /**
+     * Get the OrderBy mappings from the Model
+     *
+     * @return array
+     */
+    public function orderByMaps();
 }
--- src/Contracts/QueriableContract.php
+++ src/Contracts/QueriableContract.php
@@ -2,7 +2,23 @@
 
 namespace JoshWhatK\Eloquent\Extensions\Contracts;
 
+use Illuminate\Database\Eloquent\Builder;
+
 interface QueriableContract
 {
-  //
+    /**
+     * Get the column mappings from the Model
+     *
+     * @return array
+     */
+    public function columnMaps();
+
+    /**
+     * A fallback search method if the mappings fail
+     *
+     * @param  Illuminate\Database\Eloquent\Builder $query
+     * @param  string $search                              $request->get('query')
+     * @return Illuminate\Database\Eloquent\Builder
+     */
+    public function fullSearch(Builder $query, $search);
 }
